19738b5de4562d8680eccc3d855d8414759e8cb1
echozyr2001 <echo.zyr.2001@gmail.com>
2025-05-10 23:28:15 +0800

    Removed submodule openai-api

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,7 +1,5 @@
-mod call_llm;
 mod call_llm_async;
 mod stream_llm;
 
-pub use call_llm::call_llm_chat;
 pub use call_llm_async::call_llm_async;
 pub use stream_llm::{StreamLlmOptions, call_llm_streaming, convert_json_to_chat_messages};
